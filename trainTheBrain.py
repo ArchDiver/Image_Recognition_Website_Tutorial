27c99daf66991925a1e17b1769a3bc2be41a753c
Leland S Crawford <lelandscrawford@gmail.com>
2021-08-12 17:44:25 -0400

    adding trainTheBrain.py

---
+++ trainTheBrain.py
@@ -0,0 +1,4 @@
+from keras.models import Sequential
+from keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense, Dropout
+import tensorflow as tf
+from tensorflow.keras import backend as K
